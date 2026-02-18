b8d53c5672134feb3951bda7d0b278a51c4d3633
BritneyNi <soporteti@picso.com.co>
2026-02-17 19:19:18 -0500

    feat: initialize Spring Boot project with basic configuration and structure

---
+++ britneynino/src/main/java/application/BritneyninoApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritneyninoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BritneyninoApplication.class, args);
+	}
+
+}
